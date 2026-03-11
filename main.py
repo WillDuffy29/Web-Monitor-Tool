22e10d6d9feffbced4f2c83527744f52e05ebd3a
Will Duffy <will_duffy@icloud.com>
2026-03-11 18:28:03 +0000

    Add main.py - program entry point with monitoring loop

---
+++ main.py
@@ -0,0 +1,32 @@
+from checker import check_website
+from logger import save_log
+import time
+import json
+
+with open("sites.json", "r") as file:
+    sites = json.load(file)
+
+def start():
+    answer = input("Would you like to begin the program? (Y/N): ")
+    if answer == "Y":
+        next_answer = input("This program runs every 2 minutes until it is stopped. Are you sure? (Y/N): ")
+        if next_answer == "Y":
+            return run_program()
+        elif next_answer == "N":
+            return start()
+        else:
+            print("Invalid input. Please enter Y or N.")
+            return start()
+    
+    if answer == "N":
+        return print("No worries, maybe next time!")
+
+def run_program():
+    while True:
+        for site in sites:
+            result = check_website(site)
+            save_log(result)
+            print(result)
+        time.sleep(120)
+
+start()
